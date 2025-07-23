e5a5fc2d8a44e24d1338db20d2c0470214003f3f
SkyeCat <skyecat2024@gmail.com>
2025-07-23 00:09:08 -0500

    updated to run in a loop until told to exit

--- analysis.py
+++ analysis.py
@@ -4,39 +4,50 @@ import os
 script_dir = os.path.dirname(__file__)
 data_path = os.path.join(script_dir, "data.json")
 
-with open(data_path, "r") as file:
-    data = json.load(file)
-
 while True:
-    weapon_name = input("Enter Weapon Display Name: ").strip()
 
-    if weapon_name not in data:
-        print("Weapon not found. Please try again")
+    with open(data_path, "r") as file:
+        data = json.load(file)
+
+    while True:
+        weapon_name = input("Enter Weapon Display Name: ").strip()
+
+        if weapon_name not in data:
+            print("Weapon not found. Please try again")
+        else:
+            break
+
+    print("\nCurrent Attributes:")
+    for attribute, value in data[weapon_name].items():
+        print(f"    {attribute}: {value}")
+
+    attribute = input("\nEnter Attribute to change or add: ").strip()
+    current_value = data[weapon_name].get(attribute, "<Not Set>")
+    print(f"Current Atttribute Value: {current_value}")
+
+    new_value = input("Enter New Value: ").strip()
+
+    # Convert to number if possible
+    try:
+        if "." in new_value:
+            new_value = float(new_value)
+        else:
+            new_value = int(new_value)
+    except ValueError:
+        pass  # Leave as string
+
+    data[weapon_name][attribute] = new_value
+
+    with open(data_path, "w") as file:
+        json.dump(data, file, indent=2)
+
+    print(f"\n{attribute} updated to {new_value} for '{weapon_name}'")
+
+    run_again = input("\nRun Again? [y/n]: ")
+    
+    if run_again == ("y" or "Y" or "yes" or "Yes" or "YES"):
+        continue
     else:
         break
 
-print("\nCurrent Attributes:")
-for attribute, value in data[weapon_name].items():
-    print(f"    {attribute}: {value}")
-
-attribute = input("\nEnter Attribute to change or add: ").strip()
-current_value = data[weapon_name].get(attribute, "<Not Set>")
-print(f"Current Atttribute Value: {current_value}")
-
-new_value = input("Enter New Value: ").strip()
-
-# Convert to number if possible
-try:
-    if "." in new_value:
-        new_value = float(new_value)
-    else:
-        new_value = int(new_value)
-except ValueError:
-    pass  # Leave as string
-
-data[weapon_name][attribute] = new_value
-
-with open(data_path, "w") as file:
-    json.dump(data, file, indent=2)
-
-print(f"\n{attribute} updated to {new_value} for '{weapon_name}'")
+print("Program has exited")
